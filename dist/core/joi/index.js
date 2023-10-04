ea07b4c6ddbbaaf92cf41cc48ce5db385b845c51
Giang Vo <tragiang@bacoor.co>
2023-10-04 15:11:42 +0700

    init project

--- dist/core/joi/index.js
+++
@@ -1,4 +0,0 @@
-import JoiLib from "joi";
-import JoiObjectId from "joi-objectid";
-const Joi = Object.assign(Object.assign({}, JoiLib), { objectId: JoiObjectId(JoiLib) });
-export default Joi;
--- dist/core/utils/index.js
+++
@@ -1,7 +0,0 @@
-import { ethers } from "ethers";
-export function trimslash(s) {
-    return s[s.length - 1] === "/" ? s.slice(0, s.length - 1) : s;
-}
-export const randomString = (numberCharacter) => {
-    return `${ethers.hexlify(ethers.randomBytes(numberCharacter)).toUpperCase()}`;
-};
